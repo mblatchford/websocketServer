c015cec57a967e08382d5c076739bad6f80b2b45
Michael Blatchford <mblatchford@mac.com>
2019-01-10 16:41:51 -0500

    on connection user is staged to pendingMatch, with every two a unique room is generated

--- ws.js
+++ ws.js
@@ -1,10 +1,30 @@
 let WebSocketServer = require('ws').Server;
 let wss = new WebSocketServer({port: 3001});
 const WebSocket = require('ws');
+let uuid = require('uuid/v4');
 
+let pendingMatch = [];
+let arrOfRooms = [];
+class rooms{
+    constructor(id, p1, p2){
+      this.id = id;
+      this.p1 = p1;
+      this.p2 = p2;
+    }
+}
 
 wss.on("connection", function connection(ws){
-  console.log("client connected")
+    console.log("Connection made to server")
+    // on each connection add to pendingMatch array
+    pendingMatch.push(ws)
+    // for every two connections generate a room and reinitialize the pendingMatch arr
+    if (pendingMatch.length == 2){
+    console.log("pair connected")
+    let generateRoom = new rooms(uuid(), pendingMatch[0], pendingMatch[1]);
+    arrOfRooms.push(generateRoom)
+    pendingMatch = [];
+    console.log(arrOfRooms.length)//num of rooms present
+    }
   ws.on("message", function incoming(message){
        
     wss.clients.forEach(function each(client) {
