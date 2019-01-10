89224e802ad657ee4571d8179ca50ad66fdb4ef9
Michael Blatchford <mblatchford@mac.com>
2019-01-10 18:19:53 -0500

    added default turn handling for each room

--- ws.js
+++ ws.js
@@ -6,10 +6,9 @@ let uuid = require('uuid/v4');
 let pendingMatch = [];
 let arrOfRooms = [];
 class rooms{
-    constructor(id, p1, p2){
+    constructor(id, players){
       this.id = id;
-      this.p1 = p1;
-      this.p2 = p2;
+      this.players = players
     }
 }
 
@@ -20,16 +19,28 @@ wss.on("connection", function connection(ws){
     // for every two connections generate a room and reinitialize the pendingMatch arr
     if (pendingMatch.length == 2){
     console.log("pair connected")
-    let generateRoom = new rooms(uuid(), pendingMatch[0], pendingMatch[1]);
-    arrOfRooms.push(generateRoom)
+    let generatedRoom = new rooms(uuid(), pendingMatch);
+    arrOfRooms.push(generatedRoom)
     pendingMatch = [];
     console.log(arrOfRooms.length)//num of rooms present
+      generatedRoom.players[0].send(JSON.stringify({type: 'roomInit', roomId: generatedRoom.id, turn: true}))
+      generatedRoom.players[1].send(JSON.stringify({type: 'roomInit', roomId: generatedRoom.id, turn: false}))
     }
   ws.on("message", function incoming(message){
-       
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+    parsedMessage = JSON.parse(message)
+    console.log(parsedMessage)
+    let filteredRooms = arrOfRooms.filter(roomObj => {
+        if (roomObj.id === parsedMessage.id){
+          return true;
+        }else{
+          return false;
+        }
+      })
+      console.log('filteredRooms')
+      console.log(filteredRooms)
+      filteredRooms[0].players.forEach(function each(player) {
+      if (player !== ws && player.readyState === WebSocket.OPEN) {
+        player.send(JSON.stringify(parsedMessage));
       }
     });
   });
